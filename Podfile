c6bdae2434556ae434354f4301a703bc011eb95a
SwiftCode <SwiftCode@gamil.com>
2020-07-10 18:36:51 +0800

    1

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'gMapCurrent' do
+  pod 'GooglePlaces'
+  pod 'GoogleMaps'
+end
--- gMapCurrent/ViewController.swift
+++ gMapCurrent/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: Properties
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var zoomLevel: Float = 15.0
+    
+let defaultLocation = CLLocation(latitude: 40.730610, longitude: -73.935242)
+    
+    
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Initialize the location manager.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.delegate = self
+
+
+        
+        // Create a map.
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                              longitude: defaultLocation.coordinate.longitude,
+                                              zoom: zoomLevel)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.settings.myLocationButton = true
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.isMyLocationEnabled = true
+
+        // Add the map to the view, hide it until we've got a location update.
+        view.addSubview(mapView)
+        
+        locationManager.startUpdatingLocation()
+        
     }
 
+    // MARK: Location Manager Delegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        //print("Location: \(location)")
+
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+
+    
+        mapView.animate(to: camera)
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+          print("Location access was restricted.")
+        case .denied:
+          print("User denied access to location.")
+          // Display the map using the default location.
+          mapView.isHidden = false
+        case .notDetermined:
+          print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+          print("Location status is OK.")
+        @unknown default:
+          fatalError()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+          locationManager.stopUpdatingLocation()
+          print("Error: \(error)")
+    }
+    
 
 }
 
