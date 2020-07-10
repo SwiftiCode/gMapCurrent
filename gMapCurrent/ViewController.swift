6bf827ff4a72bf258b1401b5f505f88e0ca1077e
SwiftCode <SwiftCode@gamil.com>
2020-07-10 16:55:06 +0800

    Initial Commit

---
+++ gMapCurrent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gMapCurrent
+//
+//  Created by SwiftiCode on 10/7/20.
+//  Copyright © 2020 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
